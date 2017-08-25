58fa341938fa7b4b2e254af8fa3b5feb2c4554dd
adoublebarrel <alex@tttchicken.com>
2017-08-25 14:17:15 +0100

    Initial customisation

---
+++ personal/init.el
@@ -0,0 +1,8 @@
+(prelude-require-packages '(
+                            py-gnitset
+                            py-autopep8
+                            pycoverage
+                            pyenv-mode-auto
+                            ))
+
+'(py-gnitset-global-mode)
